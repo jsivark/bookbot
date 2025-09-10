1c13780b81298cb142c0c0db0c6ed2a2f1d16b18
quantum-sage <65615807+quantum-sage@users.noreply.github.com>
2025-09-10 11:52:51 +0530

    added stats and taking input from command line

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from stats import count_num_words,get_book_text
-
+from stats import count_num_words,get_book_text,count_chars, sort_on
+import sys
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+    else:
+        path = sys.argv[1]
     text = get_book_text(path)
     word_count = count_num_words(text)
-    print(f"{word_count} words found in the document")
+    stats = count_chars(text)
+    sort_out = sort_on(stats)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for i in sort_out:
+        if i[1].isalpha():
+            print(f"{i[1]}: {i[0]}")
+    print("============= END ===============")
+ 
+    
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
@@ -11,8 +11,13 @@ def count_num_words(text:str) -> int:
 def count_chars(text:str) -> Dict:
     text = text.split()
     vocabulary = {}
-    unique_values = set(text)
-    for c in text:
-        
+    for word in text:
+        for c in word.lower():
+            vocabulary[c] = vocabulary.get(c,0)+1
+    return vocabulary #sorted(((v,k) for k,v in vocabulary.items()), reverse= True)
+
+def sort_on(stats:Dict) -> str:
+    sort_out = sorted(((v,k) for k,v in stats.items()), reverse = True)
+    return sort_out
 
         
